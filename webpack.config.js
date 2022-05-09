9cce25d7d70e7b938598a749c20fbc172297d3a6
Straight To <straighttojanestreet@rs.school>
2022-05-09 20:06:57 +0300

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const EslintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'Virtual Keyboard'
+    }),
+    new EslintPlugin()
+  ]
+}
